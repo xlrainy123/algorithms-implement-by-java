99bf1af8addad1cd2a62393bc7a9477dbcd822dd
xlcheng <2414592951@qq.com>
2018-07-13 09:51:02 +0800

    两个栈实现一个队列

--- src/main/java/两个栈实现一个队列/Solution.java
+++ src/main/java/两个栈实现一个队列/Solution.java
@@ -1,4 +1,35 @@
 package 两个栈实现一个队列;
 
+
+import java.util.Stack;
+
 public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (!stack2.isEmpty()){
+            return stack2.pop();
+        }else {
+            while (!stack1.isEmpty()){
+                int node = stack1.pop();
+                stack2.push(node);
+            }
+            return stack2.pop();
+        }
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        solution.push(1);
+        solution.push(2);
+        solution.push(3);
+
+        System.out.println(solution.pop());
+        System.out.println(solution.pop());
+    }
 }
