27ce75041cadbd410ba739f560836833a9ace545
xlcheng <2414592951@qq.com>
2018-07-14 13:03:32 +0800

    树的子结构 && 数组的奇偶数分离

---
+++ src/main/java/将数组中的奇数偶数分离/Solution.java
@@ -0,0 +1,40 @@
+package 将数组中的奇数偶数分离;
+
+public class Solution {
+
+    //这种方法是不稳定的算法
+    public void reOrderArray(int[] array) {
+        /**
+        int low = 0, high = array.length - 1;
+        while (low < high){
+            while(low < high && array[low] % 2 == 1){
+                low++;
+            }
+            while(low < high && array[high] % 2 == 0){
+                high--;
+            }
+            if (low < high){
+                int t = array[low];
+                array[low] = array[high];
+                array[high] = t;
+            }
+        }
+        **/
+        int[] tmp = new int[array.length];
+        int i = 0;
+        for (int a : array){
+            if (a % 2 == 1){
+                tmp[i++] = a;
+            }
+            System.out.println(tmp[i-1]);
+        }
+        for (int a : array){
+            if (a % 2 == 0){
+                tmp[i++] = a;
+            }
+            System.out.println(tmp[i-1]);
+        }
+        array = tmp;
+    }
+
+}
---
+++ src/main/java/树的子结构/Solution.java
@@ -0,0 +1,34 @@
+package 树的子结构;
+
+import tree.TreeNode;
+
+public class Solution {
+
+    public boolean HasSubtree(TreeNode root1, TreeNode root2){
+
+        if (root1 == null || root2 == null){
+            return false;
+        }
+
+        boolean result = false;
+        if (root1.val == root2.val){
+            result = helper(root1,root2);
+        }
+        if (!result){
+            result = HasSubtree(root1, root2);
+        }
+        if (!result){
+            result = HasSubtree(root1, root2);
+        }
+        return result;
+    }
+
+    public boolean helper(TreeNode root1, TreeNode root2){
+
+        if (root2 == null){return true;}
+        if (root1 == null){return false;}
+        if (root1.val != root2.val){return false;}
+        return helper(root1.left, root2.left) && helper(root1.right, root2.right);
+
+    }
+}
