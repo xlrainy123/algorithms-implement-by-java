61faab5bbb5c6fc9350728443d6060370a881528
xlrainy123 <2414592951@qq.com>
2019-04-11 11:12:45 +0800

    添加bst删除操作算法导论中的实现

--- src/main/java/tree/bst/BSTree.java
+++ src/main/java/tree/bst/BSTree.java
@@ -173,8 +173,140 @@ public class BSTree {
         return null;
     }
 
+    private boolean isLeaf (TreeNode node){
+        if (node == null) {
+            throw new NoSuchElementException("节点为空");
+        }
+        return node.left == null && node.right == null;
+    }
+
+    /**
+     * 自己写的delete，还存在一些问题
+     * @param root
+     * @param target
+     * @param <E>
+     * @return
+     */
     public <E extends Comparable> boolean delete(TreeNode root, E target){
-        throw new UnsupportedOperationException();
+        System.out.println("target: "+target);
+        System.out.println("root: "+root);
+        TreeNode parent = null;
+        TreeNode current = root;
+        while (current != null ){
+            if (target.compareTo(current.element) > 0){
+                parent = current;
+                current = current.right;
+            } else if (target.compareTo(current.element) < 0){
+                parent = current;
+                current = current.left;
+            } else {
+                System.out.println("break current:"+current);
+                break;
+            }
+        }
+        if (current == null){
+            System.out.println(parent);
+            throw new NoSuchElementException("待删除节点"+new TreeNode<>(target)+"不存在!");
+        }
+        System.out.println("current:"+current);
+        System.out.println("parent:"+parent);
+        /**
+         * 待删除的节点没有孩子节点，那么直接删除就可以
+         */
+        if (isLeaf(current)){
+            if (parent.left == current){
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+            current.parent = null;
+            return true;
+        }
+        /**
+         * 待删除的节点只有一个孩子，那么把这个孩子提升到待删除节点的位置代替它
+         */
+        boolean isLeft = parent.left == current ? true : false;         //删除的节点是根节点的时候，这个parent == null
+        if ((current.right == null && current.left != null) ||
+                (current.left == null && current.left == null)){
+            TreeNode child = current.left != null ? current.left : current.right;
+            if (isLeft){
+                parent.left = child;
+            }else {
+                parent.right = child;
+            }
+            child.parent = parent;
+            current.parent = null;
+            current.left = null;
+            current.right = null;
+            return true;
+        }
+
+        /**
+         * 待删除的节点有两个孩子节点
+         */
+        TreeNode successor = successorOf(current.element, root);
+        System.out.println("successor:"+successor);
+        current.element = successor.element;
+        System.out.println("successor.element:"+successor.element);
+        //如果这个后继是待删除节点的右孩子的话
+        if (current.right == successor){
+            current.right = successor.right;
+            successor.right.parent = current;
+            successor.parent = null;
+            successor.right = null;
+            return true;
+        }
+        //如果这个后继不是待删除节点的右孩子的话
+        //这个后继的父亲一定在待删除节点的右子树上，并且，这个后继一定是父亲的左孩子，后继一定只有右孩子
+        TreeNode preParent = successor.parent;
+        System.out.println("preParent: "+preParent);
+        if (successor.right != null){
+            successor.right.parent = preParent;
+            preParent.left = successor.right;
+            successor.parent = null;
+            successor.right = null;
+        } else {
+            preParent.left = null;
+            successor.parent = null;
+            successor.right = null;
+        }
+        return true;
+    }
+
+    public <E extends Comparable> TreeNode deleteByAlgorithmInductionWithEle(TreeNode root, E target){
+        return deleteByAlgorithmInductionWithNode(root, search_1(root, target));
+    }
+
+    public TreeNode deleteByAlgorithmInductionWithNode(TreeNode root, TreeNode target){
+        if (target.left == null){
+           return transplate(root, target, target.right);
+        }
+        if (target.right == null){      // 只有左子树，没有右子树
+            return transplate(root, target, target.left);
+        }
+        TreeNode directSuccessor = getMinNode(target.right);
+        target.element = directSuccessor.element;
+        return deleteByAlgorithmInductionWithNode(root, directSuccessor);
+    }
+
+    private void nulled(TreeNode target){
+        target.parent = null;
+        target.left = null;
+        target.right = null;
+    }
+
+    private TreeNode transplate(TreeNode root, TreeNode transplated, TreeNode transplate){
+        if (transplated.parent == null){
+            root = transplate;
+        } else if (transplated.parent.left == transplated){
+            transplated.parent.left = transplate;
+        } else {
+            transplated.parent.right = transplate;
+        }
+        if (transplate != null){
+            transplate.parent = transplated.parent;
+        }
+        return root;
     }
 
     public TreeNode insert(TreeNode root, TreeNode node){
@@ -220,11 +352,21 @@ public class BSTree {
          *          1   3     5    9
          *                        8
          */
-        System.out.println(bsTree.search_2(root, 9));
-        System.out.println("getMinNode:"+bsTree.getMinNode(root));
-        System.out.println("getMaxNode:"+bsTree.getMaxNode(root));
-        System.out.println("successorOf(8, root):"+bsTree.successorOf(8, root));
-        System.out.println("successorOf(8, root):"+bsTree.preDecessorOf(5, root));
+//        System.out.println(bsTree.search_2(root, 9));
+//        System.out.println("getMinNode:"+bsTree.getMinNode(root));
+//        System.out.println("getMaxNode:"+bsTree.getMaxNode(root));
+//        System.out.println("successorOf(8, root):"+bsTree.successorOf(8, root));
+//        System.out.println("successorOf(8, root):"+bsTree.preDecessorOf(5, root));
+        TreeNode node = bsTree.search_1(root,7);
+        root = bsTree.deleteByAlgorithmInductionWithEle(root, 7);
+        bsTree.preOrder(root);
+        System.out.println();
+        bsTree.inOrder(root);
+        System.out.println();
+        System.out.println(node);
+        System.out.println(node.parent);
+        System.out.println(node.left);
+        System.out.println(node.right);
     }
 
     public void preOrder(TreeNode root){
